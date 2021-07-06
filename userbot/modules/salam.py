5d2a9603e51675ce9a1694d33686f8ca977746e4
nishfu22 <85071999+nishfu22@users.noreply.github.com>
2021-07-07 02:15:10 +0800

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -10,7 +10,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^.P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb sayang.`")
+    await typew.edit("`Assalamu'alaikum wr. wb Nama gua nishfu IM salam kenal semua 👋.`")
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
